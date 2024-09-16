41ba01d9222d51f34eac5f2551bca63eddc49d02
syeef <syeefislam@yahoo.com>
2024-09-16 19:12:14 +0530

    intial setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Server is runing");
+});
+
+app.listen(5000, console.log("Server is started on port 5000"));
